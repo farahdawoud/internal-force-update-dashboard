86cc275d879545d83793bca9354090671a21b658
farahdawoud <121869547+farahdawoud@users.noreply.github.com>
2023-03-13 16:57:24 +0400

    Added dummy upgrade messages

---
+++ src/data/upgradeMessages.ts
@@ -0,0 +1,20 @@
+const messages = [
+  {
+    message:
+      "Hey there! We've just released a new update for our app and we really hope you'll install it as soon as possible. This update includes some great new features and improvements, as well as bug fixes and stability enhancements. So please, install the update and enjoy the best experience.",
+  },
+  {
+    message:
+      "Hey there! We wanted to let you know that we've just released a new update for our app. This update includes some great new features and improvements, so we highly recommend you update to the latest version as soon as possible.Thanks, and we hope you enjoy the new update!",
+  },
+  {
+    message:
+      "Hey! You should update our app because we've fixed some bugs and made some improvements. Plus, it's always good to have the latest and greatest features.",
+  },
+  {
+    message:
+      "Hi! We're excited to let you know about our latest update. This update includes bug fixes and performance improvements to make your experience better.So, why should you update?- To keep your experience smooth and problem-free- To get the latest features",
+  },
+];
+
+export default messages;
