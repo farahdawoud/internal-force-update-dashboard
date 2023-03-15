899b245546171c48133c9741e0783266edd2e005
farahdawoud <121869547+farahdawoud@users.noreply.github.com>
2023-03-15 12:38:55 +0400

    Creating form with proper styling

--- src/components/NewVersionForm/NewVersionForm.styles.ts
+++ src/components/NewVersionForm/NewVersionForm.styles.ts
@@ -1,16 +1,35 @@
 import { Theme } from "@mui/material/styles";
 import { createStyles, makeStyles } from "@mui/styles";
+import colors from "../../ui/theme/colors";
 
 export default makeStyles((theme: Theme) =>
   createStyles({
     mainContainer: {
-      backgroundColor: "white",
+      backgroundColor: colors.white,
+      borderRadius: 10,
+      padding: 18,
     },
     buttonsContainer: {
       display: "flex",
       flexDirection: "row",
-      paddingBlock: 100,
-      justifyContent: "center",
+      paddingTop: 30,
+      justifyContent: "space-between",
+      alignSelf: "center",
+      marginInline: 300,
+    },
+    horizontalDiv: {
+      display: "flex",
+      marginBottom: 18,
+    },
+    generateMsgButton: {
+      backgroundColor: "transparent",
+    },
+    togglesContainer: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      paddingInline: 8,
     },
   })
 );
--- src/components/NewVersionForm/NewVersionForm.view.tsx
+++ src/components/NewVersionForm/NewVersionForm.view.tsx
@@ -1,27 +1,104 @@
-import { Button } from "@mui/material";
-import React from "react";
+import { Alert, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { ButtonsContainer } from "./ButtonsContainer/ButtonsContainer";
 import useStyles from "./NewVersionForm.styles";
+import { TogglesContainer } from "./TogglesContainer/TogglesContainer";
 
-export const AddVersionFormView = ({
-  addVersionHandler,
+export const NewVersionFormView = ({
+  submitButtonHandler,
   closeForm,
+  data,
+  setData,
+  generateMessageHandler,
+  errorMessage,
 }: {
-  addVersionHandler: any;
+  submitButtonHandler: any;
   closeForm: any;
+  setData: any;
+  data: any;
+  generateMessageHandler: any;
+  errorMessage: string;
 }) => {
   const styles = useStyles();
 
+  const [focusedInput, setFocusedInput] = useState(false);
+
+  console.log("focused", focusedInput);
+
   return (
     <div className={styles.mainContainer}>
-      Form here
-      <div className={styles.buttonsContainer}>
-        <Button variant="text" onClick={closeForm}>
-          Cancel
+      <h3>Add Version:</h3>
+      <form>
+        <div className={styles.horizontalDiv}>
+          <TextField
+            id="outlined-basic"
+            placeholder="App Name"
+            value={data.appName}
+            onFocus={() => setFocusedInput(true)}
+            style={{ flex: 1, marginInlineEnd: 18 }}
+            onChange={(e) => setData({ ...data, appName: e.target.value })}
+          />
+          <TextField
+            id="outlined-basic"
+            placeholder="App Version - Example: 1.0.0"
+            value={data.version}
+            onFocus={() => setFocusedInput(true)}
+            style={{ flex: 1 }}
+            onChange={(e) => setData({ ...data, version: e.target.value })}
+          />
+        </div>
+        <div className={styles.horizontalDiv}>
+          <TextField
+            id="outlined-basic"
+            placeholder="Platform"
+            value={data.platform}
+            onFocus={() => setFocusedInput(true)}
+            style={{ flex: 1, marginInlineEnd: 18 }}
+            onChange={(e) =>
+              setData({ ...data, platform: e.target.value.toUpperCase() })
+            }
+          />
+          <TextField
+            id="outlined-basic"
+            placeholder="Environment"
+            value={data.environment}
+            onFocus={() => setFocusedInput(true)}
+            style={{ flex: 1 }}
+            onChange={(e) =>
+              setData({ ...data, environment: e.target.value.toUpperCase() })
+            }
+          />
+        </div>
+
+        <TextField
+          id="outlined-basic"
+          placeholder="Update Message"
+          multiline={true}
+          onFocus={() => setFocusedInput(true)}
+          rows={3}
+          value={data.updateMessage}
+          style={{ flex: 1, width: "100%", marginInlineEnd: 10 }}
+          onChange={(e) => setData({ ...data, updateMessage: e.target.value })}
+        />
+        <Button
+          className={styles.generateMsgButton}
+          onClick={() => generateMessageHandler()}
+        >
+          Generate message
         </Button>
-        <Button variant="outlined" onClick={addVersionHandler}>
-          Add Version
-        </Button>
-      </div>
+        <TogglesContainer data={data} setData={setData} />
+      </form>
+      {errorMessage != "" && !focusedInput && (
+        <Alert style={{ marginTop: 10 }} severity="error">
+          {errorMessage}
+        </Alert>
+      )}
+
+      <ButtonsContainer
+        closeForm={closeForm}
+        submitButtonHandler={submitButtonHandler}
+        setFocusedInput={setFocusedInput}
+      />
     </div>
   );
 };
