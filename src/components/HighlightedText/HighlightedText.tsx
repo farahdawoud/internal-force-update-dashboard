f5128254281299572a69738ef2d67e8d1ec592e3
farahdawoud <121869547+farahdawoud@users.noreply.github.com>
2023-03-15 12:40:20 +0400

    Added colored prop in highlighted text

--- src/components/HighlightedText/HighlightedText.tsx
+++ src/components/HighlightedText/HighlightedText.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./HighlightedText.css";
 
-export const HighlightedText = ({ text }: { text: string }) => {
+export const HighlightedText = ({
+  text,
+  colored,
+}: {
+  text: string;
+  colored?: boolean;
+}) => {
   return (
     <div
-      className="highlight"
-      //   style={{
-      //     backgroundColor: "lightgrey",
-      //     width: "30%",
-      //     borderRadius: 10,
-      //   }}
+      className="version-number"
+      style={{ backgroundColor: colored ? "#ccffeb" : "rgb(234, 231, 231)" }}
     >
-      <p className="version-number">{text}</p>
+      <p>{text}</p>
     </div>
   );
 };
