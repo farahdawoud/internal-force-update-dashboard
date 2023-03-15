1d9e12182b7ce54558ae75f47225c26e59611488
farahdawoud <121869547+farahdawoud@users.noreply.github.com>
2023-03-15 12:39:04 +0400

    Changing apis url

--- src/networking/Apis.ts
+++ src/networking/Apis.ts
@@ -1,4 +1,8 @@
-export default {
-  addVersion: "/app-version",
-  getAllVersions: "/app-version",
+const Apis = {
+  addVersion: "/dashboard/app-version",
+  editVersion: "/dashboard/app-version/version/id/",
+  deleteVersion: "/dashboard/app-version/version/id/",
+  getAllVersions: "/dashboard/app-version",
 };
+
+export default Apis;
