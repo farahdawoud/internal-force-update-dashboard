9d8f704dbd6e55c51d110658766ea70695e33036
farahdawoud <121869547+farahdawoud@users.noreply.github.com>
2023-03-30 12:55:56 +0400

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,61 @@
 import "./App.css";
 import Header from "./components/Header";
 import { HomePage } from "./pages/HomePage/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPageController } from "./pages/LoginPage/LoginPage.controller";
+import { CookiesProvider } from "react-cookie";
+import { ApiKey } from "./components/ApiKey/ApiKey";
+import VersionsManagementPage from "./pages/VersionsManagementPage";
+import { ProtectedRoute } from "./pages/ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HomePage />
-    </div>
+    <CookiesProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route
+                path="home"
+                element={
+                  <>
+                    <Header />
+                    <HomePage />
+                  </>
+                }
+              />
+              <Route
+                path="home/versions"
+                element={
+                  <ProtectedRoute>
+                    <Header />
+                    <VersionsManagementPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="home/api-key"
+                element={
+                  <ProtectedRoute>
+                    <Header />
+                    <ApiKey />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/auth"
+                element={
+                  <>
+                    <Header />
+                    <LoginPageController />
+                  </>
+                }
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </CookiesProvider>
   );
 }
 
