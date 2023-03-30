b9938485670f2560d69868d58c97f707dd9fb022
farahdawoud <121869547+farahdawoud@users.noreply.github.com>
2023-03-30 17:12:59 +0400

    Home button in header

--- src/components/Header/Header.view.tsx
+++ src/components/Header/Header.view.tsx
@@ -4,9 +4,11 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { Cookies } from "react-cookie";
+import { useLocation } from "react-router-dom";
 const cookies = new Cookies();
 
 const Header = () => {
+  const location = useLocation();
   const token = cookies.get("token");
   console.log("TOKEN", token);
 
@@ -22,6 +24,11 @@ const Header = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Internal Upgrade Portal
           </Typography>
+          {location.pathname !== "/" && (
+            <Button style={{ marginInlineEnd: 30 }} href="/" color="inherit">
+              Home
+            </Button>
+          )}
           {token && (
             <Button href="/auth" color="inherit" onClick={() => logout()}>
               Logout
