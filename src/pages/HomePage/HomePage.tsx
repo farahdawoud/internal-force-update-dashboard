963003b23b6bb7813e1e861c3f9d3efacff8ec72
farahdawoud <121869547+farahdawoud@users.noreply.github.com>
2023-03-30 12:58:34 +0400

    HomePage refactoring

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,52 @@
-import { MainController } from "./MainController";
+import { useNavigate, Navigate } from "react-router-dom";
+import { Cookies } from "react-cookie";
+import { Button } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import "./HomePage.css";
+import colors from "../../ui/theme/colors";
 
-export const HomePage = () => {
+const Home = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <MainController />
+    <div className="navigation-buttons-container">
+      <div className="sub-container">
+        <Button
+          style={{
+            color: "white",
+            backgroundColor: colors.lightBlue,
+            paddingInline: 30,
+            width: "100%",
+            borderRadius: 30,
+            paddingBlock: 10,
+          }}
+          sx={{ boxShadow: 10 }}
+          endIcon={<ArrowForwardIcon />}
+          onClick={() => navigate("versions")}
+        >
+          Versions
+        </Button>
+        <Button
+          endIcon={<ArrowForwardIcon />}
+          sx={{ boxShadow: 10 }}
+          style={{
+            color: "white",
+            backgroundColor: "#8E4162",
+            width: "100%",
+            borderRadius: 30,
+            paddingBlock: 10,
+          }}
+          onClick={() => navigate("api-key")}
+        >
+          API Key
+        </Button>
+      </div>
     </div>
   );
+};
+
+export const HomePage = () => {
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+
+  return <div>{token ? <Home /> : <Navigate to="/auth" />}</div>;
 };
---
+++ src/pages/VersionsManagementPage/index.ts
@@ -0,0 +1,2 @@
+import { VersionsManagementPage } from "./VersionsManagementPage";
+export default VersionsManagementPage;
